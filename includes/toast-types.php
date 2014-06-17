e5c2179a5d23e605290b4fe433ad4abdd51a058b
Toast Design <inf0@toastdesign.nl>
2014-06-17 10:30:26 +0200

    Create custom post type

--- includes/toast-types.php
+++ includes/toast-types.php
@@ -1,12 +1,22 @@
 <?php 
 
-add_action( 'after_setup_theme', 'toast_types' );
-function toast_types() {
-    if ( ! class_exists( 'Super_Custom_Post_Type' ) )
-        return;
+    add_action( 'after_setup_theme', 'toast_types' );
+    function toast_types() {
+        if ( ! class_exists( 'Super_Custom_Post_Type' ) )
+            return;
 
-    // Set custom post type
-    $homes = new Super_Custom_Post_Type( 'home', 'Home', 'Homes' );
-}
+        // Set custom post type(s)
+        $huizen = new Super_Custom_Post_Type( 'huis', 'Huis', 'Huizen' );
+        $foos = new Super_Custom_Post_Type( 'foo', 'Foo', 'Foos' );
+
+        // Set custom taxonomy(s)
+        $steden = new Super_Custom_Taxonomy( 'stad', 'Stad', 'Steden', 'category' );
+        $straten = new Super_Custom_Taxonomy( 'straat', 'Straat', 'Straten', 'category' );
+        $tags = new Super_Custom_Taxonomy( 'tag', 'Tag', 'Tags' );
+        
+        // Connect taxonomy met custom post type(s)
+        connect_types_and_taxes( $huizen, array($steden, $straten, $tags) );
+
+    }
 
 ?>
